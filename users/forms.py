73cc1cb0c827422e06bf7cdc25f275aa28d11c2d
PanVat <panvatmail@gmail.com>
2025-11-14 09:48:24 +0100

    Dokončení stylování uživatelských formulářů

--- users/forms.py
+++ users/forms.py
@@ -49,17 +49,12 @@ class CustomUserCreationForm(UserCreationForm):
         }),
     )
 
-    # Potvrzení hesla
-    password2 = forms.CharField(
-        label=_("Confirm password"),
-        widget=forms.PasswordInput(attrs={
-            'class': INPUT_CLASSES,
-            'placeholder': _('Confirm your password')
-        }),
-    )
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Odstraníme pole pro potvrzení hesla
+        del self.fields['password2']
+
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_class = 'space-y-4'  # Přidá mezeru mezi poli
@@ -67,24 +62,25 @@ class CustomUserCreationForm(UserCreationForm):
         # Nastavíme třídy pro labely
         self.helper.label_class = LABEL_CLASSES
 
+        # Ikonky do vstupních polí
+        self.fields['username'].widget.attrs.update({'icon': 'user'})
+
         # Vlastní CSS styly pro pole formuláře
         self.helper.layout = Layout(
             Field('username', css_class=INPUT_CLASSES),
             Field('email', css_class=INPUT_CLASSES),
             Field('password1', css_class=INPUT_CLASSES),
-            Field('password2', css_class=INPUT_CLASSES),
-            Submit('submit', _("Sign up"),
-                   css_class="w-full py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 text-lg font-medium")
+            Submit('submit', _("Sign up"))
         )
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email__iexact=email).exists():
-            raise forms.ValidationError(_("Email address is already in use."))
+            raise forms.ValidationError(_("This email is already in use."))
         return email
 
 
@@ -121,8 +117,7 @@ class CustomLoginForm(forms.Form):
         self.helper.layout = Layout(
             Field('username'),
             Field('password'),
-            Submit('submit', _("Log in"),
-                   css_class="w-full py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 text-lg font-medium")
+            Submit('submit', _("Log in"))
         )
 
     def clean(self):
